2850b8837a238398d7702369d4b20cfc19761a53
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-07-29 23:02:32 +0500

    update posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
-  has_many :likes
+  has_many :likes 
+  after_save: :post_counter
+  def post_counter
+    comments.order(created_at: :desc).limit(5)
+  end
+  private 
+  def post_counter
+    author.update(post_counter: :author.posts.count)
+  end
 end
