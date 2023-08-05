ca6be6f4c6e9b37e1e50db0b0b28ae04de01cb84
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-08-06 03:26:38 +0500

    update project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,10 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
+    @comments = Comment.all
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:id])
+    @post = Post.find(params[:id])
   end
 end
