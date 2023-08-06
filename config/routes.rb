dfd81695a552965022bf076c8eb6d0ecd6535ba2
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-08-06 10:47:00 +0500

    update changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create] 
-      resources :likes, only: [:create]
+  resources :users do
+    resources :posts do
+      resources :comments, only: [:create, :new]
     end
   end
 end
