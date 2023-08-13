114ffde7044a4b2ec64a561d3bf41996b5a2cd76
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-08-13 09:17:56 +0500

    delete files

--- test/models/add_posts_to_like_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AddPostsToLikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/add_users_to_post_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AddUsersToPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
